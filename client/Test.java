a18c3f426d11aaf151598bd0bcb793edb6257248
yema-esipe <lima.mathieu@gmail.com>
2020-02-26 14:11:13 +0100

    Troisième commit test

--- client/Test.java
+++ client/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ceci est un test");
+		System.out.println("Deuxième modif");
 	}
 
 }

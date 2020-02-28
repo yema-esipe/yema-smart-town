5189f0a5ad8d3bc14e9706d6822d4e34fe09c750
ElisaFOLLET <elisa3.follet@gmail.com>
2020-02-28 14:10:19 +0100

    1

--- src/connection/DataSource.java
+++ src/connection/DataSource.java
@@ -1,5 +1,23 @@
 package connection;
 
 public class DataSource {
-
+	private static JDBCConnectionPool connection;
+	
+	public static JDBCConnectionPool getConnection() { //give to the client a connection available 
+		try {
+			connection.getConnection();
+		} catch (Exception e) {}		
+	}
+	
+	public static void putConnection() {
+		try {
+			connection.putConnection();
+		} catch (Exception ee) {}
+	}
+	
+	public static void closeAllConnection() {
+		try {
+			connection.closeAllConnection();
+		} catch (Exception e) {}
+	}
 }

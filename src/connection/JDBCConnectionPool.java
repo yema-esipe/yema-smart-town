2ae6c5e97fda7adfe6d1bf0070f6ecb51b10ce34
K2ya <60603990+K2ya@users.noreply.github.com>
2020-02-28 14:17:00 +0100

    JDBC class new

--- src/connection/JDBCConnectionPool.java
+++ src/connection/JDBCConnectionPool.java
@@ -4,7 +4,25 @@ import java.sql.Connection;
 import java.util.ArrayList;
 
 public class JDBCConnectionPool {
-	ArrayList<Connection> connections = new ArrayList<Connection>();
+	private ArrayList<Connection> connections = new ArrayList<Connection>();
+	
+	JDBCConnectionPool() {
+		
+	}
+	
+	public Connection getConnection () {
+		Connection tempConnection = connections.get(0); 
+		connections.remove(0);
+		return tempConnection;
+	}
+	
+	public void putConnection (Connection c) {
+		connections.add(c);
+	}
+	
+	public void closeAllConnections() {
+		
+	}
 	
 	
 }

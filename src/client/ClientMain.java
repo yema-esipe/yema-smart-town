5e91d3227499bd64b03350d562c3d1e7e1404f2e
K2ya <60603990+K2ya@users.noreply.github.com>
2020-02-28 15:25:15 +0100

    Properties File Commit

---
+++ src/client/ClientMain.java
@@ -0,0 +1,13 @@
+package client;
+
+import common.PropertiesFileReader;
+
+public class ClientMain {
+
+	public static void main(String[] args) {
+		PropertiesFileReader propertiesFileReader = new PropertiesFileReader();
+		propertiesFileReader.init();
+
+	}
+
+}
---
+++ src/common/PropertiesFileReader.java
@@ -0,0 +1,30 @@
+package common;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesFileReader {
+	private  Properties applicationProperties;
+
+	public PropertiesFileReader() {
+		applicationProperties = new Properties();
+	}
+	
+	public void init() {
+		  try (InputStream input = getClass().getClassLoader().getResourceAsStream("ressources/application.properties")) {
+
+	            // load a properties file
+			  applicationProperties.load(input);
+			  System.out.println(getProperty("driver"));
+	           
+	        } catch (IOException ex) {
+	            ex.printStackTrace();
+	        }
+	}
+	
+	public String getProperty(String key) {
+		return applicationProperties.getProperty(key);
+	}
+
+
+}

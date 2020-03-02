6a94a176bf12ec3d73313d98a4e9185c8d58d5df
ElisaFOLLET <elisa3.follet@gmail.com>
2020-03-02 14:32:34 +0100

    import inutile

--- src/connection/JDBCConnectionPool.java
+++ src/connection/JDBCConnectionPool.java
@@ -3,7 +3,6 @@ package connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 import common.PropertiesFileReader;
